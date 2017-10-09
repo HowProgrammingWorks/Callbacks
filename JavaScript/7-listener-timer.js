003360b1524412d2ba2945d806954ca002515745
tshemsedinov <timur.shemsedinov@gmail.com>
2017-10-09 21:42:27 +0300

    Improve listener example

--- JavaScript/7-listener-timer.js
+++ JavaScript/7-listener-timer.js
@@ -1,7 +1,17 @@
 'use strict';
 
-const fn = () => {
-  console.log('Next 5 seconds');
+const iterate = (array, listener) => {
+  let counter = 0;
+  setInterval(() => {
+    listener(array[counter++]);
+    if (counter >= array.length) counter = 0;
+  }, 5000);
 };
 
-setInterval(fn, 5000);
+const cities = ['Kiev', 'London', 'Beijing'];
+
+const fn = (city) => {
+  console.log('Next city: ' + city);
+};
+
+iterate(cities, fn);
