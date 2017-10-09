94fe3a3d1e5151a9e23ff84445837fefc342b4d0
tshemsedinov <timur.shemsedinov@gmail.com>
2017-10-09 21:45:40 +0300

    Add error handling example

---
+++ JavaScript/b-errors.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// Implementation
+
+const adder = (value) => {
+  const add = (a) => {
+    value += a;
+    if (value >= add.maxValue) {
+      setImmediate(() => {
+        add.maxEvent(new Error('max value reached'), value);
+      });
+    }
+    return add;
+  };
+  // callback-last
+  add.max = (max, event) => {
+    add.maxValue = max;
+    add.maxEvent = event;
+    return add;
+  };
+  return add;
+};
+
+// Usage
+
+// error-first
+const maxReached = (err, value) => {
+  if (err) {
+    console.log('value: ' + value);
+    throw err;
+  }
+};
+
+try {
+  const a1 = adder(10).max(100, maxReached)(-5);
+  a1(25);
+  a1(50);
+  a1(75);
+  a1(100);
+  a1(-200)(50)(30);
+} catch (e) {
+  console.log('Never');
+}
+
+console.log('end');
