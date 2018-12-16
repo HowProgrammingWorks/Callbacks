94e14beafcf8ce552ba261e825e591835f1d63b4
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-16 13:19:23 +0200

    Better Deferred example

--- JavaScript/a-deferred.js
+++ JavaScript/a-deferred.js
@@ -1,24 +1,57 @@
 'use strict';
 
-const getConferences = () => {
-  let onDone = null;
-  const deferred = {
-    onData: callback => onDone = callback
-  };
-  setTimeout(() => {
-    if (onDone) onDone(['Tehran', 'Yalta', 'Potsdam']);
-  }, 5000);
-  return deferred;
-};
+class Deferred {
+  constructor() {
+    this.value = undefined;
+    this.error = null;
+    this.finished = false;
+    this.events = {};
+  }
+
+  resolve(value) {
+    if (this.finished) return;
+    this.value = value;
+    this.finished = true;
+    const event = this.events['done'];
+    if (event) event.forEach(fn => fn(value));
+  }
+
+  reject(err) {
+    if (this.finished) return;
+    this.error = err;
+    this.finished = true;
+    const event = this.events['fail'];
+    if (event) event.forEach(fn => fn(err));
+  }
+
+  done(fn) {
+    if (this.finished) return;
+    const event = this.events['done'];
+    if (event) event.push(fn);
+    else this.events['done'] = [fn];
+    return this;
+  }
+
+  fail(fn) {
+    if (this.finished) return;
+    const event = this.events['fail'];
+    if (event) event.push(fn);
+    else this.events['fail'] = [fn];
+    return this;
+  }
+}
 
 // Usage
 
-const conferences = getConferences();
+const conferences = new Deferred()
+  .done(list => {
+    console.log(list);
+  })
+  .fail(err => {
+    throw err;
+  });
 
 console.log(conferences);
 
-conferences.onData(list => {
-  console.log(list);
-});
-
-console.log('end');
+conferences.resolve(['Tehran', 'Yalta', 'Potsdam']);
+conferences.reject(new Error('Never occurs'));
