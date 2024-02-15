00f480d1296b4f145759f362875936ed936c59d3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-02-15 11:07:27 +0200

    Add examples with node:timers/promises

--- JavaScript/4-callback-timer.js
+++ JavaScript/4-callback-timer.js
@@ -7,3 +7,16 @@ const fn = () => {
 const timeout = (interval, fn) => setTimeout(fn, interval);
 
 timeout(5000, fn);
+
+// Node timers promises
+
+(async () => {
+  const { setTimeout } = require('node:timers/promises');
+
+  setTimeout(1000, 'Hello').then((data) => {
+    console.log(data);
+  });
+
+  await setTimeout(1000);
+  console.log('Done');
+})();
